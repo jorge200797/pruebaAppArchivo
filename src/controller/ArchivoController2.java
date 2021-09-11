ff4682a3979070b75db3e1f44aa4d5b0fd4bab96
jorge200797 <jorge200797@gmail.com>
2021-09-11 13:23:50 -0500

    ingreso de informe sin idArchivo

--- src/controller/ArchivoController2.java
+++ src/controller/ArchivoController2.java
@@ -44,7 +44,7 @@ public class ArchivoController2 implements Initializable {
     //variables fx
     @FXML
     private TextField txtIdArchivo;
-    
+
     @FXML
     private JFXComboBox<String> txtInIdArchJco;
 
@@ -85,6 +85,9 @@ public class ArchivoController2 implements Initializable {
     private JFXButton btnRinforme;
 
     @FXML
+    private JFXButton btnRinformeSinTomo;
+
+    @FXML
     private JFXButton btnNfolio;
 
     //
@@ -94,16 +97,17 @@ public class ArchivoController2 implements Initializable {
     java.sql.ResultSet resultSetC = null;
     PreparedStatement preparedStatementC = null;
     private String combonombreArchivoSql;
-    
+
     private int idGenerado;
 
-    String validacionCampoVacio="Campo vacio";
-     String validacionLongitud="Se requiere una longitud de 8";
-       String validacionNumeros="Ingresar solo numeros";
-        String ingresoArchivoCorrecto="Se registro correctamente el archivo";
-         String ingresoInformeCorrecto="Se registro correctamente el informe";
-         
-         Boolean esCorrecto;
+    String validacionCampoVacio = "Campo vacio";
+    String validacionLongitud = "Se requiere una longitud de 8";
+    String validacionNumeros = "Ingresar solo numeros";
+    String ingresoArchivoCorrecto = "Se registro correctamente el archivo";
+    String ingresoInformeCorrecto = "Se registro correctamente el informe";
+   String ingresoInformeSinTomoCorrecto = "Se registro correctamente el informe sin tomo";
+    Boolean esCorrecto;
+
     public ArchivoController2() {
         ConnectionUtil connectionUtil = new ConnectionUtil();
         connection = (Connection) connectionUtil.getConnection();
@@ -117,37 +121,37 @@ public class ArchivoController2 implements Initializable {
     private void añadirArchivo(ActionEvent event) throws SQLException {
         int id = 0;
         int idGenerado;
-         validaAñadirArchivo(event);
-         
-                  if(esCorrecto==false){
-                 //validaAñadirArchivo(event);
-                  }
-                  else{
-                      //esCorrecto=true;
-                      
-        try {
-            ConnectionUtil connectionUtil = new ConnectionUtil();
-            connection = (Connection) connectionUtil.getConnection();
+        validaAñadirArchivo(event);
 
-            String sql = "INSERT INTO tb_archivo (nombreArchivo) VALUES " + "('" + ingresarNombreArchivo() + "')";
-            preparedStatement = (PreparedStatement) connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            if (resultSet.next()) {
-                this.idGenerado = resultSet.getInt(1);
-                System.out.println("ID Autogenerado:  " + this.idGenerado);
+        if (esCorrecto == false) {
+            //validaAñadirArchivo(event);
+        } else {
+            //esCorrecto=true;
+
+            try {
+                ConnectionUtil connectionUtil = new ConnectionUtil();
+                connection = (Connection) connectionUtil.getConnection();
+
+                String sql = "INSERT INTO tb_archivo (nombreArchivo) VALUES " + "('" + ingresarNombreArchivo() + "')";
+                preparedStatement = (PreparedStatement) connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                if (resultSet.next()) {
+                    this.idGenerado = resultSet.getInt(1);
+                    System.out.println("ID Autogenerado:  " + this.idGenerado);
+
+                }
+                preparedStatement.executeUpdate();
+                resultSet.close();
+
+                preparedStatement.close();
+
+                infoBox(ingresoArchivoCorrecto, "info", null);
+
+            } catch (Exception e) {
+                e.printStackTrace();
 
             }
-            preparedStatement.executeUpdate();
-            resultSet.close();
-
-            preparedStatement.close();
-            
-              infoBox(ingresoArchivoCorrecto, "info", null);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        }}
+        }
     }
 
     public String ingresarNombreArchivo() {
@@ -157,36 +161,26 @@ public class ArchivoController2 implements Initializable {
     }
 
     //
-  
     @FXML
 
-    private void añadirInforme(ActionEvent event) throws SQLException {
+    private void añadirInformeSinIdArchivo(ActionEvent event) throws SQLException {
         int id = 0;
         int idGenerado;
-        validaAñadirIforme(event);
-                  if(esCorrecto==false){
-                 esCorrecto=false;
-                  
-                  }
-                  
-                  else{
-                                           // esCorrecto=true;
- esCorrecto=false;
+        //validaAñadirIforme(event);
+
+        // esCorrecto=true;
         try {
             ConnectionUtil connectionUtil = new ConnectionUtil();
             connection = (Connection) connectionUtil.getConnection();
-              if (txtInNfolios.getText().isEmpty()){
-              
-              }
-            String sql = "INSERT INTO `tb_informe` (      `FECHA`, `DOCUMENTO`, `ASUNTO`, `REMITENTE`, `AREAADERIVAR`, `FECHADERECEPCCION`, `N°DEFOLIOS`, `idArchivo`) VALUES ("
+        
+            String sql = "INSERT INTO `tb_informe_sinidarchivo` (      `FECHA`, `DOCUMENTO`, `ASUNTO`, `REMITENTE`, `AREAADERIVAR`, `FECHADERECEPCCION`, `N°DEFOLIOS`, `idArchivo`) VALUES ("
                     + "'" + ingresaFecha() + "' "
                     + ",'" + ingresaDocumento() + "'"
                     + ",'" + ingresaAsunto() + "'"
                     + ",'" + ingresaRemitente() + "'"
                     + ",'" + ingresaAreaaderivar() + "'"
                     + ",'" + ingresaFechaderepccion() + "'"
-                    + ",'" + ingresatNdefolios() + "'"
-                    + ",'" + idArchivo()+ "')";
+                    + ",'" + ingresatNdefolios()+"'," +0+")";
 
             preparedStatement = (PreparedStatement) connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
@@ -199,61 +193,103 @@ public class ArchivoController2 implements Initializable {
             resultSet.close();
 
             preparedStatement.close();
-                 infoBox(ingresoInformeCorrecto, "info", null);
+            infoBox(ingresoInformeSinTomoCorrecto, "info", null);
         } catch (Exception e) {
             e.printStackTrace();
 
-        }}
+        }
 
     }
-    
-      public static void infoBox(String infoMessage, String titleBar, String headerMessage) {
+
+    @FXML
+
+    private void añadirInforme(ActionEvent event) throws SQLException {
+        int id = 0;
+        int idGenerado;
+        validaAñadirIforme(event);
+        if (esCorrecto == false) {
+            esCorrecto = false;
+
+        } else {
+            // esCorrecto=true;
+            esCorrecto = false;
+            try {
+                ConnectionUtil connectionUtil = new ConnectionUtil();
+                connection = (Connection) connectionUtil.getConnection();
+             
+                String sql = "INSERT INTO `tb_informe` (      `FECHA`, `DOCUMENTO`, `ASUNTO`, `REMITENTE`, `AREAADERIVAR`, `FECHADERECEPCCION`, `N°DEFOLIOS`,) VALUES ("
+                        + "'" + ingresaFecha() + "' "
+                        + ",'" + ingresaDocumento() + "'"
+                        + ",'" + ingresaAsunto() + "'"
+                        + ",'" + ingresaRemitente() + "'"
+                        + ",'" + ingresaAreaaderivar() + "'"
+                        + ",'" + ingresaFechaderepccion() + "'"
+                        + ",'" + ingresatNdefolios() + "'"
+                        + ",'" + idArchivo() + "')";
+
+                preparedStatement = (PreparedStatement) connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                if (resultSet.next()) {
+                    this.idGenerado = resultSet.getInt(1);
+                    System.out.println("ID Autogenerado:  " + this.idGenerado);
+
+                }
+                preparedStatement.executeUpdate();
+                resultSet.close();
+
+                preparedStatement.close();
+                infoBox(ingresoInformeCorrecto, "info", null);
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+        }
+
+    }
+
+    public static void infoBox(String infoMessage, String titleBar, String headerMessage) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(titleBar);
         alert.setHeaderText(headerMessage);
         alert.setContentText(infoMessage);
         alert.showAndWait();
     }
-      @FXML
-     public void validaAñadirArchivo(ActionEvent event){
-         
-             if(txtnArchivo.getText().trim().isEmpty()){
-                  esCorrecto=false;
-              infoBox(validacionCampoVacio, "info", null);
-              
-             
-              }
-             else{
-                 esCorrecto=true;
+
+    @FXML
+    public void validaAñadirArchivo(ActionEvent event) {
+
+        if (txtnArchivo.getText().trim().isEmpty()) {
+            esCorrecto = false;
+            infoBox(validacionCampoVacio, "info", null);
+
+        } else {
+            esCorrecto = true;
 //              
-             }
-             
-           
-   
-   }
-       @FXML
-     public void validaAñadirIforme(ActionEvent event){
-                 String documento=txtInDocumento.getText().trim().toString();
-                 int longitudDocumento=documento.length();
-                  System.out.println("validar + logitud documento="+longitudDocumento);
-             LocalDate date = txtInFechaJc.getValue();
-             LocalDate date1 = txtInFechRecepJc.getValue();
-             if (txtInNfolios.getText().trim().isEmpty()|| 
-                     txtInAsunto.getText().trim().isEmpty() || 
-                     txtInDocumento.getText().trim().isEmpty() ||
-                     String.valueOf(date).equals("null") ||
-                     String.valueOf(date1).equals("null") ||
-                     filtro(txtInIdArchJco).isEmpty() ||
-                     txtInRemitente.getText().trim().isEmpty()
-                   ){
-                 
-                 esCorrecto=false;
-              infoBox(validacionCampoVacio, "info", null);
-              }
-             else{
-               
-                 esCorrecto=true;
-             }
+        }
+
+    }
+
+    @FXML
+    public void validaAñadirIforme(ActionEvent event) {
+        String documento = txtInDocumento.getText().trim().toString();
+        int longitudDocumento = documento.length();
+        System.out.println("validar + logitud documento=" + longitudDocumento);
+        LocalDate date = txtInFechaJc.getValue();
+        LocalDate date1 = txtInFechRecepJc.getValue();
+        if (txtInNfolios.getText().trim().isEmpty()
+                || txtInAsunto.getText().trim().isEmpty()
+                || txtInDocumento.getText().trim().isEmpty()
+                || String.valueOf(date).equals("null")
+                || String.valueOf(date1).equals("null")
+                || filtro(txtInIdArchJco).isEmpty()
+                || txtInRemitente.getText().trim().isEmpty()) {
+
+            esCorrecto = false;
+            infoBox(validacionCampoVacio, "info", null);
+        } else {
+
+            esCorrecto = true;
+        }
 //               if(txtInAsunto.getText().trim().isEmpty()){
 //                   esCorrecto=false;
 //              infoBox(validacionCampoVacio, "info", null);
@@ -269,11 +305,7 @@ public class ArchivoController2 implements Initializable {
 //                 
 //                 esCorrecto=true;
 //             }
-                  
-              
-              
-                   
-                 
+
 //                 
 //                 
 //                 
@@ -313,9 +345,7 @@ public class ArchivoController2 implements Initializable {
 //                esCorrecto=true;
 //                 
 //             }
-  
-   }
-     
+    }
 
     public String ingresaFecha() {
         LocalDate date = txtInFechaJc.getValue();
@@ -327,7 +357,7 @@ public class ArchivoController2 implements Initializable {
 
     public String ingresaDocumento() {
         String documento = txtInDocumento.getText();
-        System.out.println("longitud="+documento.length());
+        System.out.println("longitud=" + documento.length());
 
         return documento;
     }
@@ -354,7 +384,7 @@ public class ArchivoController2 implements Initializable {
 
         LocalDate date = txtInFechRecepJc.getValue();
         String fechaRecepcion = String.valueOf(date);
-             System.out.println(fechaRecepcion);
+        System.out.println(fechaRecepcion);
 
         return fechaRecepcion;
 
@@ -368,94 +398,82 @@ public class ArchivoController2 implements Initializable {
 
     public int ingresaIdArchivo() {
         int ingresarIdArchivo = Integer.parseInt(txtInIdArch.getText());
-        
+
         return ingresarIdArchivo;
     }
-     public void iteracionRapidaDcomboBoxes(ObservableList<String> observableList, JFXComboBox<String> comboBoxString) {
+
+    public void iteracionRapidaDcomboBoxes(ObservableList<String> observableList, JFXComboBox<String> comboBoxString) {
         for (int i = 0; i < observableList.size(); i++) {
             comboBoxString.getItems().add(observableList.get(i));
         }
 
     }
-     
-       public String filtro(JFXComboBox<String> comboBoxString) {
+
+    public String filtro(JFXComboBox<String> comboBoxString) {
 
         String resultado = comboBoxString.getSelectionModel().getSelectedItem();
 
         return resultado;
     }
-    public void obtenerArchivos(){
-    try {
+
+    public void obtenerArchivos() {
+        try {
             ConnectionUtil connectionUtil = new ConnectionUtil();
             connection = (Connection) connectionUtil.getConnection();
 
             String sql = "SELECT nombreArchivo FROM `tb_archivo`";
-            List<String> nombresArchivo= new ArrayList<>();
-               resultSet = (java.sql.ResultSet) connection.createStatement().executeQuery(sql);
-              while (resultSet.next()) {
-                 nombresArchivo.add(resultSet.getString("nombreArchivo"));
-                     
-                  
-              }
-            
-                        
-             ObservableList<String> optionsNombreArchivo
-            = FXCollections.observableArrayList(
-               nombresArchivo    
-            );
-                iteracionRapidaDcomboBoxes(optionsNombreArchivo,txtInIdArchJco);
-           
+            List<String> nombresArchivo = new ArrayList<>();
+            resultSet = (java.sql.ResultSet) connection.createStatement().executeQuery(sql);
+            while (resultSet.next()) {
+                nombresArchivo.add(resultSet.getString("nombreArchivo"));
+
+            }
+
+            ObservableList<String> optionsNombreArchivo
+                    = FXCollections.observableArrayList(
+                            nombresArchivo
+                    );
+            iteracionRapidaDcomboBoxes(optionsNombreArchivo, txtInIdArchJco);
+
         } catch (Exception e) {
             e.printStackTrace();
 
         }
-    
+
     }
-    
-    public int  idArchivo() throws Exception{
-         int idBuscado=0;
-        combonombreArchivoSql=filtro(txtInIdArchJco);
-     
-            ConnectionUtil connectionUtil = new ConnectionUtil();
-            connection = (Connection) connectionUtil.getConnection();
-      
-          
-            String sql = "SELECT id FROM `tb_archivo` WHERE nombreArchivo="+"'"+combonombreArchivoSql+"'";
-           
-               resultSet = (java.sql.ResultSet) connection.createStatement().executeQuery(sql);
-                  resultSet.next();
-                  idBuscado=resultSet.getInt("id");
-                   System.out.println(""+idBuscado);   
-                  return  idBuscado;
-                 
-                
-              
-    
-      
-              
-      
-      
+
+    public int idArchivo() throws Exception {
+        int idBuscado = 0;
+        combonombreArchivoSql = filtro(txtInIdArchJco);
+
+        ConnectionUtil connectionUtil = new ConnectionUtil();
+        connection = (Connection) connectionUtil.getConnection();
+
+        String sql = "SELECT id FROM `tb_archivo` WHERE nombreArchivo=" + "'" + combonombreArchivoSql + "'";
+
+        resultSet = (java.sql.ResultSet) connection.createStatement().executeQuery(sql);
+        resultSet.next();
+        idBuscado = resultSet.getInt("id");
+        System.out.println("" + idBuscado);
+        return idBuscado;
+
     }
+
     @FXML
     public void selecionarIdArchivo() {
-         try {
-            idArchivo(); 
+        try {
+            idArchivo();
         } catch (Exception e) {
         }
-      
+
     }
- 
- 
- 
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //colocarImagenBotones();
         obtenerArchivos();
-        
-            //idArchivo();
-     
 
-               
+        //idArchivo();
     }
 
 }
